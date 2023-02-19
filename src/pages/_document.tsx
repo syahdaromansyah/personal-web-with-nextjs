b82363eede1774b647f9d0c2e1d323865ddba7b9
Syahda Romansyah <syahdarom@gmail.com>
2023-02-19 16:53:44 +0700

    feat: set bg and text color for all pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function CustomDocument() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-slate-100 text-slate-800">
         <Main />
         <NextScript />
       </body>
