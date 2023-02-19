3c902a09da32d69e18332856ca3a663e967e3d8b
Syahda Romansyah <syahdarom@gmail.com>
2023-02-19 16:52:47 +0700

    feat: add Inter font for Tailwind classname

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const twDefaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)', ...twDefaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
