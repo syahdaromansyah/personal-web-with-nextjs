d0287abcc7df88ddb1541ac962caf366ac04448e
Syahda Romansyah <syahdarom@gmail.com>
2023-02-20 14:32:48 +0700

    feat: add head tag and head's elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <Head>
+        <title>Syahda Romansyah</title>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/next-assets/next.svg"
+        />
+      </Head>
+    </div>
+  );
 }
